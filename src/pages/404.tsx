67a1fc8275f5cdb663e4750256a0c910405498b9
jbarget <dear.justen@gmail.com>
2021-01-11 14:14:16 +0000

    adds gatsby styled-components plugin

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,7 +1,10 @@
 import React from "react";
+import styled from "styled-components";
+
+const Div = styled.div``;
 
 const Home: React.FC = () => {
-  return <div>Page not found!</div>;
+  return <Div>Page not found!</Div>;
 };
 
 export default Home;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,10 @@
 import React from "react";
+import styled from "styled-components";
+
+const Div = styled.div``;
 
 const Home: React.FC = () => {
-  return <div>Hello world!</div>;
+  return <Div>Hello world!</Div>;
 };
 
 export default Home;
